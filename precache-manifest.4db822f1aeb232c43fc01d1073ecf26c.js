a46fa7613d7054a942f60e51c7a593e96ed78905
zhouandrewc <zhou@Andrews-MacBook-Pro-37.local>
2019-02-12 14:20:39 -0800

    Updates

---
+++ precache-manifest.4db822f1aeb232c43fc01d1073ecf26c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "0fb4d0ebc3a7ff2d9041",
+    "url": "/my-app/static/js/main.0fb4d0eb.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/my-app/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "0fb4d0ebc3a7ff2d9041",
+    "url": "/my-app/static/css/main.f5678cfa.chunk.css"
+  },
+  {
+    "revision": "d04138323d60c7c685bf5df5e0d9e8a9",
+    "url": "/my-app/index.html"
+  }
+];
