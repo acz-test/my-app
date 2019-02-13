fba1db946dba43ffe42ca7896768da54b76b10bb
acz-test <zhou@Andrews-MacBook-Pro-37.local>
2019-02-12 21:06:55 -0800

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ let gentlelaborer = require('./mp3/gentlelaborer.mp3');
 let mayonnaise = require('./mp3/mayonnaise.mp3');
 let heartman = require('./mp3/heartman.mp3');
 let imagination = require('./mp3/imagination.mp3');
+let boldandbrash = require('./mp3/boldandbrash.mp3');
+let finland = require('./mp3/finland.mp3');
+let hatsir = require('./mp3/hatsir.mp3');
+let hewasnumberone = require('./mp3/hewasnumberone.mp3');
+let namesnotrick = require('./mp3/namesnotrick.mp3');
+let onlyfood = require('./mp3/onlyfood.mp3');
+let uglybarnacle = require('./mp3/uglybarnacle.mp3');
+let dirtydan = require('./mp3/dirtydan.mp3');
+let rickroll = require('./mp3/rickroll.mp3');
 
 class App extends Component {
 
@@ -62,6 +71,24 @@ class App extends Component {
         return heartman;
       case "4":
         return imagination;
+      case "5":
+        return boldandbrash;
+      case "6":
+        return finland;
+      case "7":
+        return hatsir;
+      case "8":
+        return hewasnumberone;
+      case "9":
+        return namesnotrick;
+      case "10":
+        return onlyfood;
+      case "11":
+        return uglybarnacle;
+      case "12":
+        return dirtydan;
+      case "13":
+        return rickroll;
       default:
         console.log("default");
     }
@@ -83,10 +110,26 @@ class App extends Component {
             <div>
               <button id="1" className="board" onClick={this.addSound}>1</button>
               <button id="2" className="board" onClick={this.addSound}>2</button>
-            </div>
-            <div>
               <button id="3" className="board" onClick={this.addSound}>3</button>
               <button id="4" className="board" onClick={this.addSound}>4</button>
+            </div>
+            <div>
+              <button id="5" className="board" onClick={this.addSound}>5</button>
+              <button id="6" className="board" onClick={this.addSound}>6</button>
+              <button id="7" className="board" onClick={this.addSound}>7</button>
+              <button id="8" className="board" onClick={this.addSound}>8</button>
+            </div>
+            <div>
+              <button id="9" className="board" onClick={this.addSound}>9</button>
+              <button id="10" className="board" onClick={this.addSound}>10</button>
+              <button id="11" className="board" onClick={this.addSound}>11</button>
+              <button id="12" className="board" onClick={this.addSound}>12</button>
+            </div>
+            <div>
+              <button id="13" className="board" onClick={this.addSound}>13</button>
+            </div>
+
+            <div>
               <div id="sequence">{this.state.sequence.map((i,j) => {
                 return (
                   <button className="list" id={"list" + j} key={j} onClick={this.removeSound}>{i}</button>
