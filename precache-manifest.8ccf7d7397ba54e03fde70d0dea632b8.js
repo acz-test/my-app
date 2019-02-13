ed709e81986d390b1fe4a95ad6195e64bd598984
acz-test <zhou@Andrews-MacBook-Pro-37.local>
2019-02-12 21:08:12 -0800

    Updates

---
+++ precache-manifest.8ccf7d7397ba54e03fde70d0dea632b8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "141513de63062a48fd5c02f162e25c45",
+    "url": "/my-app/static/media/boldandbrash.141513de.mp3"
+  },
+  {
+    "revision": "b816acbad8b82440eb58",
+    "url": "/my-app/static/css/main.3eb84c34.chunk.css"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "cf08f870f21e9ad49e85",
+    "url": "/my-app/static/js/2.cf08f870.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "004f55b1f3d1faa2e1a185afa9948f01",
+    "url": "/my-app/static/media/gentlelaborer.004f55b1.mp3"
+  },
+  {
+    "revision": "a7e70fc0ad9eb1febc204c7a9c4ef6c8",
+    "url": "/my-app/static/media/mayonnaise.a7e70fc0.mp3"
+  },
+  {
+    "revision": "789e2579848bf5525572d17fed8ada84",
+    "url": "/my-app/static/media/heartman.789e2579.mp3"
+  },
+  {
+    "revision": "383a7eaf333360ffa90dc333744f9f59",
+    "url": "/my-app/static/media/imagination.383a7eaf.mp3"
+  },
+  {
+    "revision": "b816acbad8b82440eb58",
+    "url": "/my-app/static/js/main.b816acba.chunk.js"
+  },
+  {
+    "revision": "4ae5a43b8893db747e06b2d9b611e84e",
+    "url": "/my-app/static/media/finland.4ae5a43b.mp3"
+  },
+  {
+    "revision": "5208ff2aede6f56642ad1c252b3c6ed4",
+    "url": "/my-app/static/media/hatsir.5208ff2a.mp3"
+  },
+  {
+    "revision": "b46c541f154618ede7bdcf8998e3b202",
+    "url": "/my-app/static/media/hewasnumberone.b46c541f.mp3"
+  },
+  {
+    "revision": "69919ba1035d905e96672ffe1e4a7651",
+    "url": "/my-app/static/media/namesnotrick.69919ba1.mp3"
+  },
+  {
+    "revision": "bcc2b5337c7758830bf722de8b8f4d8e",
+    "url": "/my-app/static/media/onlyfood.bcc2b533.mp3"
+  },
+  {
+    "revision": "4fce7a6096342d6a08b5ade1650e9591",
+    "url": "/my-app/static/media/uglybarnacle.4fce7a60.mp3"
+  },
+  {
+    "revision": "c89d399af256c956b3967fdedfa56190",
+    "url": "/my-app/static/media/dirtydan.c89d399a.mp3"
+  },
+  {
+    "revision": "ee36d061b7345b8cbbd3157fa78ba162",
+    "url": "/my-app/static/media/rickroll.ee36d061.mp3"
+  },
+  {
+    "revision": "98c864c7f57759caec4d1654fd0d9d9a",
+    "url": "/my-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.0565b6b731e56528cce55b27a02c29b3.js"
+  "/my-app/precache-manifest.8ccf7d7397ba54e03fde70d0dea632b8.js"
 );
 
 workbox.clientsClaim();
